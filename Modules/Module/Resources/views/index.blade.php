a5be1ed0500fba4aa2cd13ac8d82c18a24d00838
Bruno <bruno@abcn.com>
2021-05-28 16:11:09 -0500

    Starting with the template

--- Modules/Module/Resources/views/index.blade.php
+++ Modules/Module/Resources/views/index.blade.php
@@ -3,15 +3,19 @@
 
 {{-- Place the content of the main page of the module --}}
 @section('content')
-    <h1>{{  __('Module') }}</h1>
+  <div class="row">
+    <div class="col-sm-12">
+      <h1>{{  __('Module') }}</h1>
 
-    <p>
-        This is the list of the modules active/inactive
-    </p>
+      <p>
+          This is the list of the modules active/inactive
+      </p>
 
-    <p>
-      @foreach ($modules as $module)
-        {{ $module['name'] }} -> {{ $module['status'] }} ->  {{ ($module['required'] == true)? 'true' : 'false' }}  ->  {{ ($module['hasConfig'] == true)? 'true' : 'false' }} <br/>
-      @endforeach
-    </p>
+      <p>
+        @foreach ($modules as $module)
+          {{ $module['name'] }} -> {{ $module['status'] }} ->  {{ ($module['required'] == true)? 'true' : 'false' }}  ->  {{ ($module['hasConfig'] == true)? 'true' : 'false' }} <br/>
+        @endforeach
+      </p>
+    </div>
+  </div>
 @endsection
